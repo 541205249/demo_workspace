046f0973f38061609fce0b8176c88de7f3785970
jzy <jiazy.gdbbk.com>
2017-05-02 21:53:16 +0800

    添加 Androidstudio重构快捷方式 文档

---
+++ refactordemo/doc/Androidstudio重构快捷方式.md
@@ -0,0 +1,19 @@
+# Androidstudio重构快捷方式
+
+1. 自动生成变量的getter、setter方法
+	
+		操作步骤：选中变量名，	Alt+Insert,选择Getter and Setter
+
+2. 改变方法签名，修改方法名字、参数顺序、参数类型和变量名
+		
+		操作步骤：选中方法名，右键->Refactor->Change Signature
+
+3. 修改匿名内部类为内部类
+
+		操作步骤：选中匿名内部类，右键->Refactor->Convert Anonymous to Inner 
+
+4. 复制一个类
+
+		操作步骤：选中要复制的类，右键->Refactor->Copy
+
+5. 泛型重构
--- refactordemo/src/main/java/com/jiazy/refactordemo/MainActivity.java
+++ refactordemo/src/main/java/com/jiazy/refactordemo/MainActivity.java
@@ -2,12 +2,27 @@ package com.jiazy.refactordemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mBtn = (Button) findViewById(R.id.btnId);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
     }
 }

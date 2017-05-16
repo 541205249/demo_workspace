d2166087fa7007c88c431668011cea3e7b18309a
jzy <jiazy.gdbbk.com>
2017-05-16 08:29:39 +0800

    添加clean code文档

---
+++ refactordemo/src/main/java/com/jiazy/refactordemo/MyLife.java
@@ -0,0 +1,49 @@
+package com.jiazy.refactordemo;
+
+/**
+ * 作者： jiazy
+ * 日期： 2017/5/13.
+ * 公司： 步步高教育电子有限公司
+ * 描述：
+ */
+public class MyLife {
+
+    public void executeDaliyLife() {
+        wakeUp();
+        haveBreakFast();
+        goToWork();
+        haveLunch();
+        goToWork();
+        haveDinner();
+        workOvertime();
+        sleep();
+    }
+
+    private void wakeUp() {
+
+    }
+
+    private void haveBreakFast() {
+
+    }
+
+    private void haveLunch() {
+
+    }
+
+    private void goToWork() {
+
+    }
+
+    private void haveDinner() {
+
+    }
+
+    private void workOvertime() {
+
+    }
+
+    private void sleep() {
+
+    }
+}
---
+++ refactordemo/src/main/java/com/jiazy/refactordemo/MyLife2.java
@@ -0,0 +1,113 @@
+package com.jiazy.refactordemo;
+
+/**
+ * 作者： jiazy
+ * 日期： 2017/5/13.
+ * 公司： 步步高教育电子有限公司
+ * 描述：
+ */
+public class MyLife2 {
+
+    public void executeDaliyLife() {
+        起床();
+        吃早餐();
+        上班();
+        下楼();
+        排队();
+        拿盘子();
+        如果盘子有点脏_换一个盘子();
+        打饭();
+        呦呵_有猪排_先打个猪排();
+        不行_猪排有点小_算了_退回去();
+        有鸡腿_再让师傅放点汤();
+        刷卡结算();
+        再拿个汤_我去_又是肥肉汤_不要();
+        开吃();
+        上班();
+        吃晚饭();
+        加班();
+        回家睡觉();
+    }
+
+    private void 起床() {
+
+    }
+
+    private void 吃早餐() {
+
+    }
+
+    private void 吃中饭() {
+        下楼();
+        排队();
+        拿盘子();
+        如果盘子有点脏_换一个盘子();
+        打饭();
+        呦呵_有猪排_先打个猪排();
+        不行_猪排有点小_算了_退回去();
+        有鸡腿_再让师傅放点汤();
+        刷卡结算();
+        再拿个汤_我去_又是肥肉汤_不要();
+        开吃();
+    }
+
+    private void 上班() {
+
+    }
+
+    private void 吃晚饭() {
+
+    }
+
+    private void 加班() {
+
+    }
+
+    private void 回家睡觉() {
+
+    }
+
+    private void 下楼() {
+
+    }
+
+    private void 排队() {
+
+    }
+
+    private void 拿盘子() {
+
+    }
+
+    private void 如果盘子有点脏_换一个盘子() {
+
+    }
+
+    private void 打饭() {
+
+    }
+
+    private void 呦呵_有猪排_先打个猪排() {
+
+    }
+
+    private void 不行_猪排有点小_算了_退回去() {
+
+    }
+
+    private void 有鸡腿_再让师傅放点汤() {
+
+    }
+
+    private void 刷卡结算() {
+
+    }
+
+    private void 再拿个汤_我去_又是肥肉汤_不要() {
+
+    }
+
+    private void 开吃() {
+
+    }
+}
